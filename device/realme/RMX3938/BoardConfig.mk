df179a027f4adae675334c3272dc0336ab4ce166
Артём <artem.malyshef.282@gmail.com>
2026-04-04 14:46:07 +0300

    Update BoardConfig.mk

--- device/realme/RMX3938/BoardConfig.mk
+++ device/realme/RMX3938/BoardConfig.mk
@@ -1,25 +1,19 @@
 DEVICE_PATH := device/realme/RMX3938
 
-# Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_VARIANT := generic
 
-# Kernel - Исправлено под твой Unisoc T612
-BOARD_KERNEL_PAGESIZE := 4096
+TARGET_BOOTLOADER_BOARD_NAME := RMX3938
+TARGET_BOARD_PLATFORM := sp9863a
+
 BOARD_BOOT_HEADER_VERSION := 4
-BOARD_KERNEL_CMDLINE := console=ttyS1,115200n8 bootconfig bootconfig
+BOARD_KERNEL_PAGESIZE := 4096
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
-# Переменные для сборки внутри BOOT (т.к. нет раздела recovery)
 BOARD_USES_RECOVERY_AS_BOOT := true
 TARGET_NO_RECOVERY := true
 
-# Partitions
-BOARD_FLASH_BLOCK_SIZE := 131072
-BOARD_HAS_LARGE_FILESYSTEM := true
-
-# TWRP Theme
 TW_THEME := portrait_hdpi
-TW_DEVICE_VERSION := Realme_Note_60x_By_Artem
+TW_DEVICE_VERSION := RMX3938_BY_ARTEM
