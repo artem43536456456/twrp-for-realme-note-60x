2b7b7eea8fd756835c0bbb11fb3aa1142de2ae3c
Артём <artem.malyshef.282@gmail.com>
2026-04-04 09:33:45 +0300

    Update twrp_RMX3938.mk

--- twrp_RMX3938.mk
+++ twrp_RMX3938.mk
@@ -1,17 +1,10 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
-
-PRODUCT_DEVICE := RMX3938
-PRODUCT_NAME := twrp_RMX3938
-PRODUCT_BRAND := realme
-PRODUCT_MODEL := Realme Note 60x
-PRODUCT_MANUFACTURER := realme
 $(call inherit-product, device/realme/RMX3938/device.mk)
 
-PRODUCT_NAME := twrp_RMX3938
 PRODUCT_DEVICE := RMX3938
+PRODUCT_NAME := twrp_RMX3938
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := Realme Note 60x
 PRODUCT_MANUFACTURER := realme
-
 PRODUCT_GMS_CLIENTID_BASE := android-realme
